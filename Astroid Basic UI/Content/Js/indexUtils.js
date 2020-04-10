7ebdf49d13524f59c9ab760783b1d2560a89dd7d
Kadir AKYIL <kadirakyil@msn.com>
2020-04-11 01:48:46 +0300

    MesajFormuEklendi

--- Astroid Basic UI/Content/Js/indexUtils.js
+++ Astroid Basic UI/Content/Js/indexUtils.js
@@ -1,19 +1,36 @@
 ﻿const left = document.querySelector(".left");
 const right = document.querySelector(".right");
 const container = document.querySelector(".container");
+const orta = document.querySelector(".ortaDirek");
 
 left.addEventListener("mouseenter", () => {
-  container.classList.add("hover-left");
+    container.classList.add("hover-left");
 });
 
 left.addEventListener("mouseleave", () => {
-  container.classList.remove("hover-left");
+    container.classList.remove("hover-left");
+});
+
+left.addEventListener("mouseenter", () => {
+    orta.classList.add("ortaLeft");
+});
+
+left.addEventListener("mouseleave", () => {
+    orta.classList.remove("ortaLeft");
 });
 
 right.addEventListener("mouseenter", () => {
-  container.classList.add("hover-right");
+    orta.classList.add("ortaRight");
 });
 
 right.addEventListener("mouseleave", () => {
-  container.classList.remove("hover-right");
+    orta.classList.remove("ortaRight");
+});
+
+right.addEventListener("mouseenter", () => {
+    container.classList.add("hover-right");
+});
+
+right.addEventListener("mouseleave", () => {
+    container.classList.remove("hover-right");
 });
